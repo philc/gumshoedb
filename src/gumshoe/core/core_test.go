a9b00daada1b4901e6ec1fdecd58d417b2328110
Phil Crosby <phil.crosby@gmail.com>
2013-12-18 21:10:39 -0800

    Write the first test

--- src/gumshoe/core/core_test.go
+++ src/gumshoe/core/core_test.go
@@ -1,10 +1,26 @@
 package core
 
-import "testing"
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func tableFixture() *FactTable {
+	return NewFactTable([]string{"col1", "col2"})
+}
 
 func TestEndToEnd(t *testing.T) {
-	table := NewFactTable([]string{"at", "country", "impressions", "clicks"})
+	t.Skip()
+	// table := NewFactTable([]string{"at", "country", "impressions", "clicks"})
+	table := tableFixture()
 	populateTableWithTestingData(table)
 	invokeQuery(table)
 	t.Errorf("fail")
+}
+
+func TestConvertRowMapToRowArray(t *testing.T) {
+	Convey("throws an error for an unrecognized column", t, func() {
+		_, error := convertRowMapToRowArray(tableFixture(), map[string]Untyped{"col1": 5, "unknownColumn": 10})
+		So(error, ShouldNotBeNil)
+	})
 }
