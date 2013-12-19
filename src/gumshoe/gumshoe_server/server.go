dfa5c1b5de7a6b756c209bf951ae751ff4ae9fdf
Phil Crosby <phil.crosby@gmail.com>
2013-12-18 21:10:55 -0800

    Support import and table dumping from a URL

--- src/gumshoe/gumshoe_server/server.go
+++ src/gumshoe/gumshoe_server/server.go
@@ -1,41 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"gumshoe/core"
-	"net/http"
-	"encoding/json"
 	"io/ioutil"
+	"net/http"
 )
 
-// func errorResponse(statusCode int, message String, responseWriter http.ResponseWriter) {
-// 	fmt.Fprintf(responseWriter
+var table = core.NewFactTable([]string{"at", "country", "impression", "clicks"})
 
-func parseJsonBody(request *http.Request) {
-	requestBody, error := ioutil.ReadAll(request.Body)
-	if error != nil {
-		fmt.Println(error)
-	}
-	jsonBody := new(core.Untyped)
-	error = json.Unmarshal([]byte(requestBody), jsonBody)
-	if error != nil {
-		fmt.Println(error)
-	}
-	fmt.Println(*jsonBody)
-}
-
-
-func handleImport(responseWriter http.ResponseWriter, request *http.Request) {
+func handleImportRoute(responseWriter http.ResponseWriter, request *http.Request) {
 	if request.Method != "PUT" {
 		http.Error(responseWriter, "", 404)
 		return
 	}
 
-	parseJsonBody(request)
+	requestBody, _ := ioutil.ReadAll(request.Body)
+	jsonBody := make([](map[string]core.Untyped), 0)
+	error := json.Unmarshal([]byte(requestBody), &jsonBody)
+	if error != nil {
+		fmt.Println(error)
+		http.Error(responseWriter, error.Error(), 500)
+	}
+
+	error = core.InsertRowMaps(table, jsonBody)
+	if error != nil {
+		fmt.Println(error)
+		http.Error(responseWriter, error.Error(), 500)
+	}
+}
+
+// TODO(philc): Make this return JSON maps of rows, rather than denormalized vectors.
+func handleTableRoute(responseWriter http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(responseWriter, "", 404)
+		return
+	}
+	jsonResult, _ := json.Marshal(table.Rows)
+	fmt.Fprint(responseWriter, string(jsonResult))
 }
 
 func main() {
 	fmt.Println(core.ROWS)
-	http.HandleFunc("/import", handleImport)
+	http.HandleFunc("/import", handleImportRoute)
+	http.HandleFunc("/tables", handleTableRoute)
 	http.ListenAndServe(":9000", nil)
 }
