97166b6f89a01e89dc15e8c347e553528b4f992e
Phil Crosby <phil.crosby@gmail.com>
2013-12-27 20:24:56 -0800

    Refactor the synthetic benchmarks to use generic types

--- src/gumshoe/benchmark/benchmark.go
+++ src/gumshoe/benchmark/benchmark.go
@@ -5,10 +5,12 @@ import "unsafe"
 import "testing"
 
 // const ROWS = 4
-const ROWS = 10000000
-const COLS = 5
-var COL_SIZE int = typeSizes["int32"]
+const ROWS = 1000000
+const COLS = 50
+var COL_SIZE int = typeSizes["float32"]
 var ROW_SIZE int = COL_SIZE * COLS
+type Cell float32
+type SumType float32
 
 var typeSizes map[string]int = map[string]int {
 	"int": asInt(unsafe.Sizeof(*new(int))),
@@ -16,88 +18,86 @@ var typeSizes map[string]int = map[string]int {
 	"int16": asInt(unsafe.Sizeof(*new(int16))),
 	"int32": asInt(unsafe.Sizeof(*new(int32))),
 	"uint32": asInt(unsafe.Sizeof(*new(uint32))),
+	"float32": asInt(unsafe.Sizeof(*new(float32))),
+	"float64": asInt(unsafe.Sizeof(*new(float64))),
 }
 
-var input = make([]*[COLS]int32, ROWS)
+var input = make([]*[COLS]Cell, ROWS)
 
-func SumArrayMatrix(matrix *[ROWS][COLS]int32) int {
+func SumArrayMatrix(matrix *[ROWS][COLS]Cell) int {
 	length := len(matrix)
-	var sum int = 0.0
+	var sum SumType = 0.0
 	for i := 0; i < length; i++ {
-		sum += int(matrix[i][0])
+		sum += SumType(matrix[i][0])
 	}
-  return sum
+  return int(sum)
 }
 
-func SumOneRolledLoop(matrix []*[COLS]int32) int {
-	var sum int = 0.0
+func SumOneRolledLoop(matrix []*[COLS]Cell) int {
+	var sum SumType = 0.0
 	length := len(matrix)
 	for i := 0; i < length; i+=1 {
-		sum += int(matrix[i][0])
+		sum += SumType(matrix[i][0])
 	}
 	return int(sum)
 }
 
-func SumSliceOfSliceMatrix(matrix [][]int32) int {
-	var sum int = 0.0
+func SumSliceOfSliceMatrix(matrix [][]Cell) int {
+	var sum SumType = 0.0
 	length := len(matrix)
 	for i := 0; i < length; i++ {
-		sum += int(matrix[i][0])
+		sum += SumType(matrix[i][0])
 	}
 	return int(sum)
 }
 
 func SumByteMatrix(matrix uintptr) int {
 	// NOTE(philc): this doesn't currently produce the correct results. It's off by a few thousand
-	sum := 0
+	var sum SumType = 0
 	length := ROWS
 	for i := 0; i < length; i++ {
-		a := *(*int32)(unsafe.Pointer(matrix))
-		sum += int(a)
+		a := *(*Cell)(unsafe.Pointer(matrix))
+		sum += SumType(a)
 		matrix += uintptr(ROW_SIZE)
 	}
-	return sum
+	return int(sum)
 }
 
-func SumOneUnrolledLoop(matrix []*[COLS]int32) int {
-	sum := 0
+func SumOneUnrolledLoop(matrix []*[COLS]Cell) int {
+	var sum SumType = 0
 	length := len(matrix)
 	for i := 0; i < length; i+=5 {
-		sum += int(matrix[i][0])
-		sum += int(matrix[i+1][0])
-		sum += int(matrix[i+2][0])
-		sum += int(matrix[i+3][0])
-		sum += int(matrix[i+4][0])
+		sum += SumType(matrix[i][0])
+		sum += SumType(matrix[i+1][0])
+		sum += SumType(matrix[i+2][0])
+		sum += SumType(matrix[i+3][0])
+		sum += SumType(matrix[i+4][0])
 	}
-	return sum
+	return int(sum)
 }
 
-func SumUsingFilter(filter func(*[5]int32) bool) int {
-	sum := 0
-	l := len(input)
-	for i := 0; i < l; i += 8 {
-		row := input[i]
-		if filter(row) {
-			sum += int(row[0])
+func SumUsingFilterFn(matrix []*[COLS]Cell, filter func(*[COLS]Cell) bool) int {
+	var sum SumType = 0
+	length := len(matrix)
+	for i := 0; i < length; i++ {
+		row := matrix[i]
+		if filter(row) && filter(row) {
+			sum += SumType(row[0])
 		}
 	}
-	return sum
+	return int(sum)
 }
 
-func SumUsingFilter2(filter func(*[5]int32) bool) int {
-	sum := 0
-	l := len(input)
-	for i := 0; i < l; i += 8 {
-		row := input[i]
-		filter(row)
-		filter(row)
-		filter(row)
-		filter(row)
-		if filter(row) {
-			sum += int(row[0])
+func SumUsingInlineFilterFn(matrix []*[COLS]Cell) int {
+	var sum SumType = 0
+	length := len(matrix)
+	for i := 0; i < length; i++ {
+		row := matrix[i]
+		if row[0] >= 0 {
+			sum += SumType(row[0])
 		}
 	}
-	return sum
+	return int(sum)
 }
 
 func SumGroupBy() int {
@@ -124,9 +124,9 @@ func asInt(p uintptr) int {
 	return *(*int)(unsafe.Pointer(&p))
 }
 
-func setValue(matrix uintptr, row int, col int, value int32) {
+func setValue(matrix uintptr, row int, col int, value Cell) {
 	matrix += uintptr((ROW_SIZE * row) + (COL_SIZE * col))
-	a := (* int32)(unsafe.Pointer(matrix))
+	a := (* Cell)(unsafe.Pointer(matrix))
 	*a = value
 }
 
@@ -143,61 +143,62 @@ func runBenchmarkFunction(name string, f func() int) {
 
 func initByteMatrix() uintptr {
 	// I don't need to allocate a slice here. I can just allocate an array.
-	matrix := *new([ROWS][COLS]int32)
+	matrix := *new([ROWS][COLS]Cell)
 	for i := 0; i < len(matrix); i++ {
 		for j := 0; j < len(matrix[i]); j++ {
-			matrix[i][j] = int32(i)
+			matrix[i][j] = Cell(i)
 		}
 	}
 	return uintptr(unsafe.Pointer(&matrix))
 }
 
-func createArrayMatrix() *[ROWS][COLS]int32 {
-	matrix := *(new([ROWS][COLS]int32))
+func createArrayMatrix() *[ROWS][COLS]Cell {
+	matrix := *(new([ROWS][COLS]Cell))
 	for i := 0; i < len(matrix); i++ {
-		matrix[i][0] = int32(i)
+		matrix[i][0] = Cell(i)
 	}
 	return &matrix
 }
 
-func initSliceMatrix() []*[COLS]int32 {
-	matrix := make([]*[COLS]int32, ROWS)
+func initSliceMatrix() []*[COLS]Cell {
+	matrix := make([]*[COLS]Cell, ROWS)
 	for i := 0; i < len(matrix); i++ {
-		matrix[i] = new([COLS]int32)
+		matrix[i] = new([COLS]Cell)
 		for j := 0; j < len(matrix[i]); j++ {
-			matrix[i][j] = int32(i)
+			matrix[i][j] = Cell(i)
 		}
 	}
 	return matrix
 }
 
-func initSliceOfSliceMatrix() [][]int32 {
-	matrix := make([][]int32, ROWS)
+func initSliceOfSliceMatrix() [][]Cell {
+	matrix := make([][]Cell, ROWS)
 	for i := 0; i < len(matrix); i++ {
-		matrix[i] = make([]int32, COLS)
+		matrix[i] = make([]Cell, COLS)
 		for j := 0; j < len(matrix[i]); j++ {
-			matrix[i][j] = int32(i)
+			matrix[i][j] = Cell(i)
 		}
 	}
 	return matrix
 }
 
 func main() {
-	byteMatrix := initByteMatrix()
+	// byteMatrix := initByteMatrix()
 	// fmt.Println(SumByteMatrix(byteMatrix))
-	runBenchmarkFunction("SumByteMatrix", func() int { return SumByteMatrix(byteMatrix) })
+	// runBenchmarkFunction("SumByteMatrix", func() int { return SumByteMatrix(byteMatrix) })
 
 	sliceMatrix := initSliceMatrix()
-	arrayMatrix := createArrayMatrix()
-	sliceOfSliceMatrix := initSliceOfSliceMatrix()
+	// arrayMatrix := createArrayMatrix()
+	// sliceOfSliceMatrix := initSliceOfSliceMatrix()
 	runBenchmarkFunction("SumOneRolledLoop", func() int { return SumOneRolledLoop(sliceMatrix) })
-	runBenchmarkFunction("SumOneUnrolledLoop", func() int { return SumOneUnrolledLoop(sliceMatrix) })
-	runBenchmarkFunction("SumArrayMatrix", func() int { return SumArrayMatrix(arrayMatrix) })
-	runBenchmarkFunction("SumSliceOfSliceMatrix", func() int { return SumSliceOfSliceMatrix(sliceOfSliceMatrix) })
+	// runBenchmarkFunction("SumOneUnrolledLoop", func() int { return SumOneUnrolledLoop(sliceMatrix) })
+	// runBenchmarkFunction("SumArrayMatrix", func() int { return SumArrayMatrix(arrayMatrix) })
+	// runBenchmarkFunction("SumSliceOfSliceMatrix", func() int { return SumSliceOfSliceMatrix(sliceOfSliceMatrix) })
 	// runBenchmarkFunction("SumMany", func() { SumMany() })
 	// runBenchmarkFunction("SumGroupBy", func() { SumGroupBy() })
 
-	// filterFunction := func(row *[5]int32) bool { return row[0] >= 0 }
-	// runBenchmarkFunction("SumUsingFilter", func() { SumUsingFilter(filterFunction) })
+	filterFunction := func(row *[COLS]Cell) bool { return row[0] >= 0 }
+	runBenchmarkFunction("SumUsingFilterFn", func() int { return SumUsingFilterFn(sliceMatrix, filterFunction) })
+	runBenchmarkFunction("SumUsingInlineFilterFn", func() int { return SumUsingInlineFilterFn(sliceMatrix) })
 	// runBenchmarkFunction("SumUsingFilter2", func() { SumUsingFilter2(filterFunction) })
 }
