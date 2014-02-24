3e42ce94d7acd88c3fc36ea5a2970434a6cc5c5b
Phil Crosby <phil@liftoff.io>
2014-02-24 00:17:51 -0800

    Add a basic test for the interval and segment allocation logic

--- src/gumshoe/core_test.go
+++ src/gumshoe/core_test.go
@@ -15,6 +15,23 @@ func tableFixture() *FactTable {
 }
 
 func TestConvertRowMapToRowArrayThrowsErrorForUnrecognizedColumn(t *testing.T) {
-	_, err := tableFixture().convertRowMapToRowArray(RowMap{"col1": 5, "at": 0, "unknownColumn": 10})
+	_, err := tableFixture().convertRowMapToRowArray(
+		RowMap{"at": 0, "col1": 5.0, "col2": "a", "unknownColumn": 10})
 	Assert(t, err, NotNil)
+}
+
+func TestNewIntervalsAreAllocatedAsNeeded(t *testing.T) {
+	table := tableFixture()
+	table.SegmentSizeInBytes = 12
+	Assert(t, len(table.Intervals), Equals, 0)
+	// This require cause a new Interval and two new segments to be allocated.
+	err := table.InsertRowMaps([]RowMap{
+		{"at": 0, "col1": 1.0, "col2": "a"},
+		{"at": 0, "col1": 2.0, "col2": "b"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	Assert(t, len(table.Intervals), Equals, 1)
+	Assert(t, len(table.Intervals[0].Segments), Equals, 2)
 }
