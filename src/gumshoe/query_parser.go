f32083fb0071d2481ed54035f24974d5127390da
Phil Crosby <phil@liftoff.io>
2014-03-04 08:03:34 -0800

    Add more error checking to queries, and only allow grouping by dimension cols

--- src/gumshoe/query_parser.go
+++ src/gumshoe/query_parser.go
@@ -35,23 +35,30 @@ type Query struct {
 	Filters    []QueryFilter
 }
 
-func isValidColumn(table *FactTable, columnName string) bool {
-	_, ok := table.ColumnNameToIndex[columnName]
-	return ok
+func isValidColumn(table *FactTable, name string) bool {
+	_, ok := table.ColumnNameToIndex[name]
+	return ok || name == table.TimestampColumnName
 }
 
+var validFilterTypes = []string{"=", "!=", ">", ">=", "<", "<=", "in"}
+
 func ValidateQuery(table *FactTable, query *Query) error {
-	for _, queryAggregate := range query.Aggregates {
-		if !isValidColumn(table, queryAggregate.Column) {
-			return fmt.Errorf("Unrecognized column name in aggregates clause: %s", queryAggregate.Column)
+	for _, aggregate := range query.Aggregates {
+		if aggregate.Type != "sum" && aggregate.Type != "average" {
+			return fmt.Errorf("Unrecognized aggregate type: %s", aggregate.Type)
 		}
-		if queryAggregate.Type != "sum" && queryAggregate.Type != "average" {
-			return fmt.Errorf("Unrecognized aggregate type: %s", queryAggregate.Type)
+		_, ok := table.Schema.MetricColumns[aggregate.Column]
+		if !ok {
+			return fmt.Errorf("Only metric columns can be used in aggregates. %s is not a metric column.",
+				aggregate.Column)
 		}
 	}
 	for _, grouping := range query.Groupings {
-		if !isValidColumn(table, grouping.Column) {
-			return fmt.Errorf("Unrecognized column name in grouping clause: %s", grouping.Column)
+
+		_, ok := table.Schema.DimensionColumns[grouping.Column]
+		if !ok {
+			return fmt.Errorf("Only dimension columns can be used in grouping. %s is not a dimension column.",
+				grouping.Column)
 		}
 		if grouping.TimeTransform != "" && grouping.TimeTransform != "minute" &&
 			grouping.TimeTransform != "hour" && grouping.TimeTransform != "day" {
@@ -63,6 +70,16 @@ func ValidateQuery(table *FactTable, query *Query) error {
 		if !isValidColumn(table, filter.Column) {
 			return fmt.Errorf("Unrecognized column name in filter clause: %s", filter.Column)
 		}
+		isValidFilter := false
+		for _, t := range validFilterTypes {
+			if t == filter.Type {
+				isValidFilter = true
+			}
+		}
+		if !isValidFilter {
+			return fmt.Errorf("%s is not a valid filter type.", filter.Type)
+		}
+
 	}
 	return nil
 }
