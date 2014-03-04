49ac88ab64e4cc1167ad539a8aaaea9190803916
Phil Crosby <phil@liftoff.io>
2014-03-04 08:01:40 -0800

    Write a basic query parser test

--- src/gumshoe/query_parser_test.go
+++ src/gumshoe/query_parser_test.go
@@ -2,13 +2,19 @@ package gumshoe
 
 import (
 	"testing"
+	. "github.com/cespare/a"
 )
 
-// TODO(philc): Write this test.
 func TestParseQuery(t *testing.T) {
-	// jsonString := `
-	// 	{"table":"the_table",
-	//    "aggregates": [{"type": "sum", "name": "clicks", "column": "country"}],
-	//    "groupings": [{"column": "country", "name":"country1"}]
-	// 	}`
+	jsonString := `
+		{"table":"the_table",
+	   "aggregates": [{"type": "sum", "name": "metric1", "column": "metric1"}],
+	   "groupings": [{"column": "dim1", "name":"grouping-name"}],
+     "filters": [{"type": "!=", "column": "at", "value": 1}]
+		}`
+	query, err := ParseJSONQuery(jsonString)
+	Assert(t, err, IsNil)
+	table := tableFixture()
+	err = ValidateQuery(table, query)
+	Assert(t, err, IsNil)
 }
