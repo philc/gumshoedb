731918ec6d8ab7e990b988954195c22dac84288a
Caleb Spare <cespare@gmail.com>
2014-03-13 10:21:22 -0700

    Add a B+tree implementation
    This is a very slight modification of github.com/cznic/b. The readme
    has notes on how to recreate.

---
+++ src/b/value.go
@@ -0,0 +1,10 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file defines the value type used in this b-tree.
+
+type MetricWithCount struct {
+	Count  int
+	Metric []byte
+}
