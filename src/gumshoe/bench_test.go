f03bd784d46f3b75c7a3de0675a5fbf6826b2454
Caleb Spare <cespare@gmail.com>
2014-04-08 19:31:44 -0700

    Enable another benchmark

--- src/gumshoe/bench_test.go
+++ src/gumshoe/bench_test.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"testing"
 	"time"
-	"unsafe"
 	"utils"
 )
 
@@ -17,14 +16,14 @@ const (
 	//tempDir          = "/tmp/gumshoe_benchmark"
 )
 
-// TODO(caleb): Look into test setup time
-//var db *DB
+var benchmarkDB *DB
 
-//func setup() {
-//if db == nil {
-//db = setUpDB()
-//}
-//}
+func setup(b *testing.B) {
+	if benchmarkDB == nil {
+		benchmarkDB = setUpDB()
+	}
+	b.SetBytes(int64(BenchmarkRows * benchmarkDB.RowSize))
+}
 
 // TODO(caleb): Change a to accept a testing.TB rather than a *testing.T so that we can use Assert instead of
 // this function (and throughout benchmarks generally).
@@ -35,41 +34,38 @@ func checkResult(b *testing.B, actual, expected interface{}) {
 	}
 }
 
+func mustGetBenchmarkQueryResult(query *Query) []RowMap {
+	results, err := benchmarkDB.GetQueryResult(query)
+	if err != nil {
+		panic(err)
+	}
+	return results
+}
+
 // A query which only sums aggregates.
 func BenchmarkAggregateQuery(b *testing.B) {
-	//setup()
-	db := setUpDB()
+	setup(b)
 	query := createBenchmarkQuery(nil, nil)
 	var results []RowMap
-	var err error
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		results, err = db.GetQueryResult(query)
-		if err != nil {
-			b.Fatal(err)
-		}
+		results = mustGetBenchmarkQueryResult(query)
 	}
 	checkResult(b, results, []RowMap{{"metric001": BenchmarkRows, "rowCount": BenchmarkRows}})
-	//setBytes(b)
 }
 
 // A query which filters rows by a single, simple filter function.
-//func BenchmarkFilterQuery(b *testing.B) {
-//setup(b)
-//// Metric 2 cycles between 0 and 1, so this will filter out 1/2 the columns.
-//query := createQuery(nil, []gumshoe.QueryFilter{{">", "metric002", 0}})
-//if err := gumshoe.ValidateQuery(factTable, query); err != nil {
-//b.Fatal(err)
-//}
-//b.ResetTimer()
-//var result map[string]gumshoe.Untyped
-//for i := 0; i < b.N; i++ {
-//result = factTable.InvokeQuery(query)
-//}
-//checkResult(b, result["results"],
-//[]map[string]int{{"metric001": BenchmarkRows / 2, "rowCount": BenchmarkRows / 2}})
-//setBytes(b)
-//}
+func BenchmarkFilterQuery(b *testing.B) {
+	setup(b)
+	// Metric 2 cycles between 0 and 1, so this will filter out 1/2 the columns.
+	query := createBenchmarkQuery(nil, []QueryFilter{{FilterGreaterThan, "metric002", 0.0}})
+	b.ResetTimer()
+	var results []RowMap
+	for i := 0; i < b.N; i++ {
+		results = mustGetBenchmarkQueryResult(query)
+	}
+	checkResult(b, results, []RowMap{{"metric001": BenchmarkRows / 2, "rowCount": BenchmarkRows / 2}})
+}
 
 //// A query which groups by a column. Each column has 10 possible values, so the result set will contain 10 row
 //// aggregates.
@@ -108,15 +104,6 @@ func BenchmarkAggregateQuery(b *testing.B) {
 //setBytes(b)
 //}
 
-//func createQuery(groupings []gumshoe.QueryGrouping, filters []gumshoe.QueryFilter) *gumshoe.Query {
-//return &gumshoe.Query{
-//TableName:  "tableName",
-//Aggregates: createQueryAggregates([]string{"metric001"}),
-//Groupings:  groupings,
-//Filters:    filters,
-//}
-//}
-
 func createBenchmarkQuery(groupings []QueryGrouping, filters []QueryFilter) *Query {
 	return &Query{
 		Aggregates: createBenchmarkQueryAggregates([]string{"metric001"}),
@@ -132,15 +119,6 @@ func createBenchmarkQueryAggregates(columns []string) []QueryAggregate {
 	}
 	return aggregates
 }
-
-//// Creates a QueryAggregate structure which represents the sums of the given columns.
-//func createQueryAggregates(columns []string) []gumshoe.QueryAggregate {
-//queryAggregates := make([]gumshoe.QueryAggregate, len(columns))
-//for i, column := range columns {
-//queryAggregates[i] = gumshoe.QueryAggregate{"sum", column, column}
-//}
-//return queryAggregates
-//}
 
 // setUpDB creates a test DB to represent a realistic schema.
 func setUpDB() *DB {
@@ -190,8 +168,4 @@ func populateBenchmarkDB(db *DB) {
 	if err := db.Insert(rows); err != nil {
 		panic(err)
 	}
-}
-
-func setBytes(b *testing.B) {
-	b.SetBytes(int64(BenchmarkRows * BenchmarkColumns * unsafe.Sizeof(float32(0))))
 }
