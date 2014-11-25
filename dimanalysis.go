baf778d4d3bae69638ecbc750cc2bda449f92a7e
Caleb Spare <cespare@gmail.com>
2014-11-25 13:49:24 -0800

    Update for new dim analysis run

--- dimanalysis.go
+++ dimanalysis.go
@@ -59,9 +59,9 @@ func runTest(dims []Dimension, name string) error {
 	}
 	//var dims []Dimension
 	//for _, d := range dimensions {
-		//if dim.Name != d.Name {
-			//dims = append(dims, d)
-		//}
+	//if dim.Name != d.Name {
+	//dims = append(dims, d)
+	//}
 	//}
 	if err := configTmpl.Execute(configFile, TestConfig{dir, dims}); err != nil {
 		return err
@@ -77,7 +77,7 @@ func runTest(dims []Dimension, name string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("%s %s %.2f%%\n", name, humanize.Bytes(size), 100.0 * float64(size)/float64(oldDBSize))
+	fmt.Printf("%s %s %.2f%%\n", name, humanize.Bytes(size), 100.0*float64(size)/float64(oldDBSize))
 	return nil
 }
 
@@ -159,23 +159,25 @@ metric_columns = [
   ["predicted_imp_to_install_rate", "float32"],
   ["predicted_imp_to_preferred_app_event_rate", "float32"],
   ["revenue", "float32"],
-  ["target", "float32"],
   ["internal_no_bid_normalized", "float32"],
   ["internal_bid", "uint32"],
-  ["internal_bid_price", "float32"]
+  ["internal_bid_price", "float32"],
+  ["reengaged_custom_app_event", "uint16"],
+  ["first_direct_custom_app_event", "uint16"],
+  ["first_reengaged_custom_app_event", "uint16"]
 ]
 `
 
 const dimensionsText = `
+  ["ab_tests", "string:uint16"],
   ["ad_group_id", "uint32"],
   ["app_genre", "string:uint16"],
   ["app_name", "string:uint32"],
   ["app_store_id", "string:uint32"],
-  ["bid_floor", "float32"],
+  ["bid_request_at", "uint32"],
   ["channel_id", "uint8"],
   ["city", "string:uint16"],
   ["click_to_install_model_tag", "string:uint8"],
-  ["connection_type", "string:uint8"],
   ["country", "string:uint8"],
   ["creative_id", "uint16"],
   ["creative_size", "string:uint16"],
@@ -185,16 +187,11 @@ const dimensionsText = `
   ["dest_app_id", "uint16"],
   ["device_family", "string:uint8"],
   ["gender", "string:uint8"],
-  ["hostname", "string:uint16"],
   ["imp_to_click_model_tag", "string:uint8"],
   ["install_to_preferred_app_event_model_tag", "string:uint8"],
-  ["javascript_supported", "uint8"],
   ["language", "string:uint16"],
-  ["model_type", "string:uint8"],
-  ["model_version", "string:uint32"],
   ["no_bid_reason_enum", "uint8"],
   ["os", "string:uint8"],
-  ["os_version", "string:uint16"],
   ["region", "string:uint16"],
   ["revenue_type", "string:uint8"],
   ["video_supported", "uint8"],
